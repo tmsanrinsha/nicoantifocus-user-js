2f0f636e4eabe2e27e855b7e9330c3782f3f14d3
tmsanrinsha <sanrinsha.tm@gmail.com>
2015-04-13 00:15:35 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# nicoantifocus.user.js
+
+ニコニコ動画などの動画サイトで、Flashプレイヤーをクリックした時にフォーカスしないようにするユーザースクリプト。
+ダブルクリックだとフォーカスする。
+VimperatorでEMBEDになって、キー制御できなくなるのを防ぐ。
+
+サイトを追加すればYoutubeなどでも使える。
+
+* [インストール](/Users/tmsanrinsha/Library/Application Support/Firefox/Profiles/uvw5iacp.default/gm_scripts/NicoAntiFocus/nicoantifocus.user.js)
+
+# オリジナル
+
+* [ニコニコ動画のプレイヤーにフォーカスしないようにするグリモン。 - 地獄の猫日記](http://d.hatena.ne.jp/nokturnalmortum/20080802/1217633913)
+
+embedタグではなくobjectタグのフォーカスを外す必要があったので、改変。
---
+++ nicoantifocus.user.js
@@ -0,0 +1,53 @@
+﻿// ==UserScript==
+// @name           NicoAntiFocus
+// @namespace      https://github.com/tmsanrinsha
+// @description    ニコニコ動画などの動画サイトで、Flashプレイヤーにフォーカスしないようにする。
+// @grant          none
+// @include        http://www.nicovideo.jp/watch/*
+// @include        http://de.nicovideo.jp/watch/*
+// @include        http://es.nicovideo.jp/watch/*
+// @include        http://tw.nicovideo.jp/watch/*
+// @downloadURL    https://raw.githubusercontent.com/tmsanrinsha/dotfiles/master/nicoantifocus.user.js
+// @updateURL      https://raw.githubusercontent.com/tmsanrinsha/dotfiles/master/nicoantifocus.user.js
+// @version        1.0
+// ==/UserScript==
+//
+// License:
+//    Creative Commons 2.1 (Attribution + Share Alike)
+//
+// Neko:
+//    nicontroller.js 用？スクリプト。
+//    うっかりプレイヤーのボタンを押すとVimperator のキーが効かなくなるので、無理矢理フォーカスしないようにする。
+//
+// Original Author:
+//    anekos
+//    http://d.hatena.ne.jp/nokturnalmortum/20080802/1217633913
+// Auhtor:
+//    tmsanrinsha
+//
+// Link:
+//    Vimperator 用の(ニコニコ動画|YouTube)プラグイン
+//    http://vimperator.kurinton.net/plugins/stella.html
+
+(function (es) {
+    var elemEmb = document.getElementsByTagName('embed');
+    var elemObj = document.getElementsByTagName('object');
+    var elems = [].concat(Array.prototype.slice.call(elemEmb), Array.prototype.slice.call(elemObj));
+
+    elems.forEach(function (elem) {
+        var doubleClick = false;
+        elem.addEventListener(
+            'focus',
+            function () {
+                if (doubleClick) {
+                    doubleClick = false;
+                } else {
+                    doubleClick = true;
+                    setTimeout(function () { doubleClick = false; }, 500);
+                    setTimeout(function () { elem.blur(); }, 0);
+                }
+            },
+            true
+        );
+    });
+})();
